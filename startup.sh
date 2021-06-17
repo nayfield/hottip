aa93a850f6992efeac6976b3ad044a71e6a5c423
rod <rod@nayfield.com>
2021-06-16 22:05:53 -0400

    make tmpdir ; fg for now

--- startup.sh
+++ startup.sh
@@ -5,6 +5,11 @@ BASE="$(dirname "$0")"
 
 cd "${BASE}"
 
+# Make hottip and tmp dir
+TD=$HOME/.hottip/tmp
+[ -d "$TD" ] || mkdir -p "$TD"
+
+# Ensure the venv is set up
 if [ ! -d ./venv ] ; then
     cat <<EOM
 ERROR! You need to set up your venv.
@@ -23,8 +28,9 @@ EOM
     exit 1
 fi
 
+# Check for commands we use
 err=0
-for need in rtxmlrpc rtcontrol git
+for need in rtxmlrpc rtcontrol git screen
 do
   which $need
   err=$(($err + $?))
@@ -35,8 +41,14 @@ if [ $err -gt 0 ] ; then
   exit 1
 fi
 
+# Default to foreground
+MODE=fg
 
-if [ "$1" = "fg" ] ; then
+if [ "$1" = "bg" ] ; then
+  MODE=bg
+fi
+
+if [ "$MODE" = "fg" ] ; then
     ${BASE}/venv/bin/python3 -m flask run
 else
     screen -d -m -S tiphandler ${BASE}/venv/bin/python3 -m flask run
