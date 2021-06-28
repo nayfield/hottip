8786884860633495cc3d7a93510b36d4b6744022
rod <rod@nayfield.com>
2021-06-28 17:28:21 -0400

    pep8 cleanup

--- handler.py
+++ handler.py
@@ -8,6 +8,7 @@ import subprocess
 
 app = Flask(__name__)
 
+
 def msg_process(msg):
     for k in msg.keys():
         sys.stdout.write(k+" : "+str(msg[k])+'\n')
@@ -15,10 +16,11 @@ def msg_process(msg):
     site = atts['Tracker']['Value']
     tid = atts['Id']['Value']
     topic = atts['Topic']['Value']
-    rcmd = ['./torload', site, tid, topic ]
-    retval = subprocess.call(rcmd)
+    rcmd = ['./torload', site, tid, topic]
+    subprocess.call(rcmd)
 
-@app.route('/signalserver', methods = ['POST'])
+
+@app.route('/signalserver', methods=['POST'])
 def sns():
 
     hdr = request.headers.get('X-Amz-Sns-Message-Type')
@@ -26,18 +28,18 @@ def sns():
     if hdr:
         js = json.loads(request.get_data())
         if hdr == 'SubscriptionConfirmation' and 'SubscribeURL' in js:
-            r = requests.get(js['SubscribeURL'])
+            requests.get(js['SubscribeURL'])
 
         if hdr == 'Notification':
             msg_process(js)
 
     return 'OK\n'
 
+
 # TODO remove eventually if running as flask run
 if __name__ == '__main__':
     app.run(
-        host = "0.0.0.0",
-        port = 31630,
-        debug = False
+        host="0.0.0.0",
+        port=31630,
+        debug=False
     )
-
