3b3aa322fd34ce73251c93f90644c43689baca6a
rod <rod@nayfield.com>
2021-06-28 17:25:34 -0400

    pep8 cleanup and bugfix

--- cleaner.py
+++ cleaner.py
@@ -7,6 +7,7 @@ import json
 import pickle
 import time
 
+
 def scmd(st):
     retval = []
     remote = False
@@ -15,8 +16,9 @@ def scmd(st):
     retval.extend(st)
     return retval
 
+
 def remove_unknowns():
-    ''' Remove dead torrents '''
+    """ Remove dead torrents """
     rcmd = ['rtcontrol', '-q', '-ohash', 'message=*Unregistered*', '--cull', '--yes']
     retval = subprocess.call(scmd(rcmd))
 
@@ -24,12 +26,12 @@ def remove_unknowns():
 
 
 def get_tors():
-    ''' return json of minimal client state'''
+    """ return json of minimal client state"""
     fields = ['hash', 'custom_getter', 'size', 'seedtime', 'alias', 'message', 'name', 'completed']
     # Some extra stuff might not be needed
     fields.extend(['custom_1', 'uploaded', 'is_complete', 'is_open', 'started', 'custom_tm_uploaded'])
 
-    rcmd = ["rtcontrol", '-q', '--json', '*', '-o'+','.join(fields) ]
+    rcmd = ["rtcontrol", '-q', '--json', '*', '-o' + ','.join(fields)]
     cout = subprocess.check_output(scmd(rcmd))
 
     # onetag = non-overlapping tags
@@ -40,54 +42,53 @@ def get_tors():
         if t['custom_getter']:
             t['onetag'] = t['custom_getter']
         elif t['custom_1']:
-            t['onetag'] = 'rutorrent:'+t['custom_1']
+            t['onetag'] = 'rutorrent:' + t['custom_1']
         else:
-            t['onetag'] = 'Alias:'+t['alias']
+            t['onetag'] = 'Alias:' + t['alias']
         retval.append(t)
     return retval
 
 
-
-
-def filterlist(torl, tag):
-    if not tag:
+def filterlist(torl, ftag):
+    if not ftag:
         return torl
     retval = []
     for t in torl:
-        if t['onetag'] == tag:
-                retval.append(t)
+        if t['onetag'] == ftag:
+            retval.append(t)
     return retval
 
-def tagusage(torl, tag):
-    ''' report GB (/1024) usage of all torrents '''
+
+def tagusage(torl, utag):
+    """ report GB (/1024) usage of all torrents """
     retval = 0
-    for t in filterlist(torl, tag):
+    for t in filterlist(torl, utag):
         retval += t['size']
-    return int(retval/1024/1024/1024)
+    return int(retval / 1024 / 1024 / 1024)
+
 
 def tags_by_overage(torl, tagl):
-    ''' return a list of (tag, over) sorted by amount over
-        input tagl[tag] = targetusage'''
+    """ return a list of (tag, over) sorted by amount over
+        input tagl[tag] = targetusage"""
     overd = {}
     for t in tagl.keys():
         overt = tagusage(torl, t) - int(tagl[t])
         if overt > 0:
             overd[t] = overt
-    return sorted(overd.items(), key=lambda x: x[1], reverse=True)
+    return sorted(overd.items(), key=lambda k: k[1], reverse=True)
 
 
-
-def get_culls(torl, tag, holdt, goal, exempt=None):
-    ''' return oldest items in torlist,
+def get_culls(torl, ctag, choldt, goal, exempt=None):
+    """ return oldest items in torlist,
          matching tag,
          seeded over holdt,
          not exceeding over
-         return tuple ([hashes], size)'''
-    fl = filterlist(torl, tag)
+         return tuple ([hashes], size)"""
+    fl = filterlist(torl, ctag)
     tl = sorted(fl, key=lambda k: k['completed'])
     culled = 0
     cutoff = int(time.time())
-    cutoff -= holdt*60*60
+    cutoff -= choldt * 60 * 60
     hashlist = []
     for t in tl:
         if culled > goal:
@@ -97,21 +98,20 @@ def get_culls(torl, tag, holdt, goal, exempt=None):
                 continue
         if t['completed'] < cutoff:
             hashlist.append(t['hash'])
-            culled += t['size']/1024/1024/1024
+            culled += t['size'] / 1024 / 1024 / 1024
 
     return hashlist, culled
 
 
-
-
 def do_culls(culll):
-    ''' cull a list of hashes'''
-    rcmd = [ 'rtcontrol', 'hash='+','.join(culll), "--cull",  "--yes" ]
+    """ cull a list of hashes"""
+    rcmd = ['rtcontrol', 'hash=' + ','.join(culll), "--cull", "--yes"]
     retval = subprocess.call(scmd(rcmd))
     return retval
 
+
 def save_state(fn, struc):
-    ''' pickle struc to fn'''
+    """ pickle struc to fn"""
     # this is for other things to look at TODO
     # TODO write out to a tmp file then move into place
     # in order to transact atomically
@@ -120,12 +120,12 @@ def save_state(fn, struc):
 
 
 if __name__ == '__main__':
-    #TODO use python-daemon?
+    # TODO use python-daemon?
 
-    mydir=os.path.expanduser('~/.hottip')
-    myconfig=os.path.join(mydir, 'cleaner.cfg')
-    stfile=os.path.join(mydir, 'torlist.pk')
-    conf=configparser.ConfigParser()
+    mydir = os.path.expanduser('~/.hottip')
+    myconfig = os.path.join(mydir, 'cleaner.cfg')
+    stfile = os.path.join(mydir, 'torlist.pk')
+    conf = configparser.ConfigParser()
     conf.read(myconfig)
 
     remove_unknowns()
@@ -160,7 +160,7 @@ if __name__ == '__main__':
             culls.extend(hashs)
         # could add another panic here if still have tgtleft
 
-        cullout = do_culls(hashs)
+        cullout = do_culls(culls)
 
         # Since we culled, grab a new torlist
         torlist = get_tors()
