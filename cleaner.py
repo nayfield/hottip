3f48e5385898b77b21bc0093e818cbd45431c70d
rod <rod@nayfield.com>
2021-06-28 20:42:48 -0400

    notes for future fun

--- cleaner.py
+++ cleaner.py
@@ -30,7 +30,11 @@ def get_tors():
     fields = ['hash', 'custom_getter', 'size', 'seedtime', 'alias', 'message', 'name', 'completed']
     # Some extra stuff might not be needed
     fields.extend(['custom_1', 'uploaded', 'is_complete', 'is_open', 'started', 'custom_tm_uploaded'])
-
+    # rtxml equiv
+    # rtxmlrpc --repr d.multicall2 '' '' d.hash= d.custom=getter d.size_bytes= d.message= d.name= d.timestamp.finished=
+    # can't get alias.
+    # workaround - after getting other vars, including d.custom=alias:
+    #   per empty alias= entries, call rtxmlrpc --repr t.multicall <hash> '' t.url= ... to get url, parse, store.
     rcmd = ["rtcontrol", '-q', '--json', '*', '-o' + ','.join(fields)]
     cout = subprocess.check_output(scmd(rcmd))
 
